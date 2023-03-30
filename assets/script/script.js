518bc9fa38f42dcb1f9adf2cb2f2f59f742f7d1e
raffytaactaac <101869732+raffytaactaac@users.noreply.github.com>
2023-03-30 15:49:02 +0800

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+function convert() {
+    let peso = parseFloat(document.getElementById("peso").value);
+    let usd = 0.018 * peso;
+    let yen = 2.43 * peso;
+    let won = 23.87 * peso;
+    let sgd = 0.024 * peso;
+    let cad = 0.024 * peso;
+
+    document.getElementById("usd").innerHTML = usd;
+    document.getElementById("yen").innerHTML = yen;
+    document.getElementById("won").innerHTML = won;
+    document.getElementById("sgd").innerHTML = sgd;
+    document.getElementById("cad").innerHTML = cad;
+}
