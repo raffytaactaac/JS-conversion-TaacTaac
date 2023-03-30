2611bffa6248d1a898333b81fbe4c7e04ada9cd7
raffytaactaac <101869732+raffytaactaac@users.noreply.github.com>
2023-03-30 16:04:40 +0800

    update cad value

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,7 @@ function convert() {
     let yen = 2.43 * peso;
     let won = 23.87 * peso;
     let sgd = 0.024 * peso;
-    let cad = 0.024 * peso;
+    let cad = 0.025 * peso;
 
     document.getElementById("usd").innerHTML = usd;
     document.getElementById("yen").innerHTML = yen;
